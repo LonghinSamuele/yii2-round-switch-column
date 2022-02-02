40da6b8dee5df7cb93596c21d1063733cc26c320
Samuele Longhin <longhin.samuele@outlook.com>
2022-02-02 09:15:24 +0100

    retrocompatibilità

--- src/components/RoundSwitchColumn.php
+++ src/components/RoundSwitchColumn.php
@@ -3,6 +3,7 @@
 /**
  * @file Render a round switch toggleColumn in Yii2 GridView.
  * @author Nick Denry
+ * @author Samuele Longhin
  */
 
 namespace samuelelonghin\grid\toggle\components;
@@ -22,62 +23,63 @@ use samuelelonghin\grid\toggle\helpers\ModelHelper;
  */
 class RoundSwitchColumn extends DataColumn
 {
-    /**
-     * @var string toggle action name
-     */
-    public $action = 'toggle';
-    public $action_params = false;
-    public $data_id_attribute = 'id';
-    public $active_param = null;
-    public $disabledSwitchTextAttribute = 'nome';
+	/**
+	 * @var string toggle action name
+	 */
+	public $action = 'toggle';
+	public $action_params = false;
+	public $data_id_attribute = 'id';
+	public $active_param = null;
+	public $disabledSwitchTextAttribute = 'nome';
 
-    /**
-     * {@inheritdoc}
-     */
-    public function init()
-    {
-        if (empty($this->filter)) {
-            $this->filter = [
-                '1' => Yii::t('yii', 'Yes'),
-                '0' => Yii::t('yii', 'No'),
-            ];
-        }
-        $this->headerOptions = ArrayHelper::merge($this->headerOptions, [
-            'data-toggle-action' => Url::toRoute([$this->action]),
-            'data-toggle-attribute' => $this->attribute,
-        ]);
-        RoundSwitchAsset::register(Yii::$app->view);
-        RoundSwitchThemeAsset::register(Yii::$app->view);
-        parent::init();
-        $this->action_params[] = $this->action;
-        $this->headerOptions = ArrayHelper::merge($this->headerOptions, [
-            'data-toggle-action' => Url::toRoute($this->action_params),
-            'data-toggle-attribute' => $this->attribute,
-        ]);
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function init()
+	{
+		if (empty($this->filter)) {
+			$this->filter = [
+				'1' => Yii::t('yii', 'Yes'),
+				'0' => Yii::t('yii', 'No'),
+			];
+		}
+		$this->headerOptions = ArrayHelper::merge($this->headerOptions, [
+			'data-toggle-action' => Url::toRoute([$this->action]),
+			'data-toggle-attribute' => $this->attribute,
+		]);
+		RoundSwitchAsset::register(Yii::$app->view);
+		RoundSwitchThemeAsset::register(Yii::$app->view);
+		parent::init();
+		$this->action_params[] = $this->action;
+		$this->headerOptions = ArrayHelper::merge($this->headerOptions, [
+			'data-toggle-action' => Url::toRoute($this->action_params),
+			'data-toggle-attribute' => $this->attribute,
+		]);
+	}
 
-    /**
-     * {@inheritdoc}
-     */
+	/**
+	 * {@inheritdoc}
+	 */
 
 
-    protected function renderDataCellContent($model, $key, $index)
-    {
-        $checked = \nickdenry\grid\toggle\helpers\ModelHelper::isChecked($model, $this->attribute);
-        $out = null;
-        if ($model instanceof PermessoToggleInterface && !$this->active_param) {
-            $active = $model->getAttivo($out);
-        } else {
-            $active = ModelHelper::isChecked($model, $this->active_param);
-        }
+	protected function renderDataCellContent($model, $key, $index)
+	{
+		$checked = \nickdenry\grid\toggle\helpers\ModelHelper::isChecked($model, $this->attribute);
+		$out = null;
+		$active = true;
+		if ($model instanceof PermessoToggleInterface && !$this->active_param) {
+			$active = $model->getAttivo($out);
+		} else if ($this->active_param) {
+			$active = ModelHelper::isChecked($model, $this->active_param);
+		}
 
-        return Yii::$app->view->render('@samuelelonghin/grid/toggle/views/switch', [
-            'model' => $model,
-            'id' => $model->getAttribute($this->data_id_attribute),
-            'checked' => $checked,
-            'name' => $this->attribute,
-            'active' => $active,
-            'disabledSwitchText' => $out->{$this->disabledSwitchTextAttribute}
-        ]);
-    }
+		return Yii::$app->view->render('@samuelelonghin/grid/toggle/views/switch', [
+			'model' => $model,
+			'id' => $model->getAttribute($this->data_id_attribute),
+			'checked' => $checked,
+			'name' => $this->attribute,
+			'active' => $active,
+			'disabledSwitchText' => $out->{$this->disabledSwitchTextAttribute}
+		]);
+	}
 }
